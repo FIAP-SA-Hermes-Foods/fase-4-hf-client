ad0941ffcd45324c9b270fbea31d9f721ad4c563
leo <leonardo.ierich07@gmail.com>
2024-05-19 21:59:49 -0300

    update service.tf
    Co-authored-by: Marcos <marcos.vp.dev88@gmail.com>

--- infrastructure/terraform/service.tf
+++ infrastructure/terraform/service.tf
@@ -4,22 +4,23 @@ resource "kubernetes_service" "hf-client-service" {
         namespace = "dev"
     }
 
-    spec { 
-        type = "LoadBalancer"
-        selector = { 
-            app = "hf-client-go-app"
-        }
-        port { 
-            protocol = "TCP"
-            name = "hf-client-http-port"
-            port = 8080
-            target_port = 8080
-        }
-        port { 
-            protocol = "TCP"
-            name = "hf-client-rpc-port"
-            port = 8070
-            target_port = 8070
-        }
+    spec {
+    type = "NodePort"
+    selector = {
+      app = "hf-client-go-app"
     }
+
+    port {
+      name        = "hf-client-http-port"
+      port        = 8080
+      target_port = 8080
+    }
+
+    port {
+      name        = "hf-client-rpc-port"
+      port        = 8070
+      target_port = 8070
+      node_port   = 30070
+    }
+  }  
 }
