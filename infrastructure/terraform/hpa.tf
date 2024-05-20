2976e3a71321ce9f64df72702746d79d2d2d59b6
leo <leonardo.ierich07@gmail.com>
2024-05-19 21:59:00 -0300

    update hpa.tf
    Co-authored-by: Marcos <marcos.vp.dev88@gmail.com>

--- infrastructure/terraform/hpa.tf
+++ infrastructure/terraform/hpa.tf
@@ -1,27 +1,49 @@
-resource "kubernetes_horizontal_pod_autoscaler" "hf-client-hpa" {
+resource "kubernetes_horizontal_pod_autoscaler_v2" "hf_client_hpa" {
   metadata {
-    name = "hf-client-hpa"
+    name      = "hf-client-hpa"
     namespace = "dev"
   }
 
   spec {
+    scale_target_ref {
+      api_version = "apps/v1"
+      kind        = "Deployment"
+      name        = "hf-client-deployment"
+    }
+
     min_replicas = 1
     max_replicas = 2
 
-    scale_target_ref {
-      kind = "Deployment"
-      name = "hf-client-deployment"
+  behavior {
+      scale_up {
+        stabilization_window_seconds = 60
+        select_policy = "Max"  # Add this line
+        policy {
+          type          = "Percent"
+          value         = 10
+          period_seconds = 60
+        }
+      }
+      scale_down {
+        stabilization_window_seconds = 120
+        select_policy = "Max"  # Add this line
+        policy {
+          type          = "Percent"
+          value         = 10
+          period_seconds = 120
+        }
+      }
     }
 
-    metric { 
-        type = "Resource"
-        resource { 
-            name = "cpu"
-            target { 
-                type = "Utilization"
-                average_utilization = 70
-            }
+    metric {
+      type = "Resource"
+      resource {
+        name = "cpu"
+        target {
+          type               = "Utilization"
+          average_utilization = 70
         }
+      }
     }
   }
 }
