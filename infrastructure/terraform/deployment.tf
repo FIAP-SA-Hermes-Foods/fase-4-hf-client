47522b5760c8dbf0c43be23f1b22a12b886680ee
leo <leonardo.ierich07@gmail.com>
2024-05-19 21:58:21 -0300

    update deployment.tf
    Co-authored-by: Marcos <marcos.vp.dev88@gmail.com>

--- infrastructure/terraform/deployment.tf
+++ infrastructure/terraform/deployment.tf
@@ -1,53 +1,74 @@
-resource "kubernetes_deployment" "hf-client-deployment" {
+resource "kubernetes_deployment" "hf_client_deployment" {
+  metadata {
+    name      = "hf-client-deployment"
+    namespace = "dev"
+  }
 
-    metadata {  
-        name = "hf-client-deployment"
-        namespace = "dev"
+  spec {
+    selector { 
+      match_labels = {
+        app = "hf-client-go-app"
+      }
     }
-    spec {  
-        selector { 
-            match_labels = {
-              app = "hf-client-go-app"
-            }
+    template { 
+      metadata { 
+        labels = {
+          app = "hf-client-go-app"
         }
-        template { 
-            metadata { 
-                labels = {  
-                    app = "hf-client-go-app"
-                }
-            }
-            spec { 
-                container { 
-                    name = "hf-client-go-app-http"
-                    image = "${data.external.env_vars.result.image_api_http_url}:${data.external.env_vars.result.image_tag}"
-                    image_pull_policy = "Always"
-                    env_from { 
-                        secret_ref { 
-                            name = "hf-deploy-secret"
-                        }
-                    }
-                    port { 
-                        container_port = 8080
-                    }
-                }
-                container { 
-                    name = "hf-client-go-app-rpc"
-                    image = "${data.external.env_vars.result.image_api_rpc_url}:${data.external.env_vars.result.image_tag}"
-                    image_pull_policy = "Always"
-                    env_from { 
-                        secret_ref { 
-                            name = "hf-deploy-secret"
-                        }
-                    }
-                    port { 
-                        container_port = 8080
-                    }
-                }
-            image_pull_secrets { 
-                    name = "hfregcred"
-                }
-            }
+      }
+      spec { 
+        image_pull_secrets { 
+          name = "hf_deploy_secret"
         }
+        
+        container { 
+          name = "hf-client-go-app-http"
+          image = "localhost:5000/app-go-http:latest"
+          image_pull_policy = "IfNotPresent"  
+          resources {
+            limits={
+                    cpu    = "500m"
+                    memory = "1Gi" 
+                }
+            requests = {
+                    cpu    = "250m"
+                    memory = "512Mi"
+                }
+            }
+          env_from { 
+            secret_ref { 
+              name = "hf-deploy-secret"
+            }
+          }
+          port { 
+            container_port = 8080
+          }
+        }
+
+        container { 
+          name = "hf-client-go-app-rpc"
+          image = "localhost:5000/app-go-rpc:latest"
+          image_pull_policy = "IfNotPresent"
+          resources {
+            limits={
+                    cpu    = "500m"
+                    memory = "1Gi" 
+                }
+            requests = {
+                    cpu    = "250m"
+                    memory = "512Mi"
+                }
+            }
+          env_from { 
+            secret_ref { 
+              name = "hf-deploy-secret"
+            }
+          }
+          port { 
+            container_port = 8080
+          }
+        }
+      }
     }
+  }
 }
-
